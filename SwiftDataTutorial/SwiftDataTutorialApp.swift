260e284e87679cdac87403d59a01c444d93e008c
vcosmin2701 <v.cosmin2717@gmail.com>
2024-11-19 11:41:09 +0200

    Initial Commit

---
+++ SwiftDataTutorial/SwiftDataTutorialApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftDataTutorialApp.swift
+//  SwiftDataTutorial
+//
+//  Created by Vladut Cosmin on 19.11.2024.
+//
+
+import SwiftUI
+
+@main
+struct SwiftDataTutorialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
